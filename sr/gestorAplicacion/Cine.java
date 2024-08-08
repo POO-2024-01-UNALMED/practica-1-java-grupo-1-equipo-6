194b52c2a30d4adb61901bc07316cb1ffdbc0d88
JhoanAlexis899 <159485809+JhoanAlexis899@users.noreply.github.com>
2024-08-08 12:04:01 -0500

    Cmabios en main y Cine

--- sr/gestorAplicacion/Cine.java
+++ sr/gestorAplicacion/Cine.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class Cine {
 	private String nombre;
 	private ArrayList<Funcion> funciones;
+	private ArrayList<Pelicula> peliculas;
 
 	public Cine(String nombre, ArrayList<Funcion> funciones){
 		this.nombre = nombre;
@@ -27,17 +28,48 @@ public class Cine {
 	
 	public boolean hayPelicula(Pelicula pelicula) {		
 		for (Funcion funcion: this.funciones){
-			if (funcion.getPelicula().getTitulo().equals(pelicula.getTitulo())){
+			if (funcion.getPelicula().getTitulo().toLowerCase().equals(pelicula.getTitulo().toLowerCase())  && this.obtenerFunciones(pelicula).size()> 0 ){
 				return true;
 			}
 		}
 		return false;
 	}
+
+	public ArrayList<Pelicula> buscarPeliculaPorNombre(String pelicula){
+		ArrayList<String> palabrasClave = new ArrayList<String>();
+		pelicula = pelicula.toLowerCase();
+		if (pelicula.indexOf(" ") == -1){
+			palabrasClave.add(pelicula);
+		} 
+		else{
+			int j = 0;
+			for(int i= 0; i<pelicula.length(); i++){
+				String palabraHallada;
+				if (String.valueOf(pelicula.charAt(i)) == " "){
+					palabraHallada = pelicula.substring(j, i-1);
+					if(palabraHallada.length()>2){
+						palabrasClave.add(palabraHallada);
+						if(i != pelicula.length() -1){j= i +1;}
+					}
+				}
+			}
+		}
+		ArrayList<Pelicula> peliculasCoincidentes = new ArrayList<Pelicula>();
+		for (int j=0; j<peliculas.size(); j++){
+			for(int y=0; y<palabrasClave.size();y++){
+				if(peliculas[j].getNombre().contains(palabrasClave.get(y))){
+					peliculasCoincidentes.add(peliculas[j]);	
+				}
+			}
+		}
+		return peliculasCoincidentes;
+	}
+
 	
 	public ArrayList<Funcion> obtenerFunciones(Pelicula pelicula){
 		ArrayList<Funcion> posibilidades = new ArrayList<Funcion>();
 		for( Funcion funcion: this.funciones){
-			if (funcion.getPelicula().getTitulo().equals(pelicula.getTitulo())){
+			if (funcion.getPelicula().getTitulo().toLowerCase().equals(pelicula.getTitulo().toLowerCase())){
 				boolean[][] lugares = funcion.getSala().getSillas();
 				for (int i = 0; i<lugares.length; i++){
 					for(int j = 0; j<lugares[0].length; i++){
@@ -57,3 +89,4 @@ public class Cine {
 	
 
 }
+
